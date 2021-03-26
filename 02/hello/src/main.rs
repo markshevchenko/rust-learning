a17dd1b3d6ddf48962fbb4286fa3722fb0b2de24
Mark Shevchenko <markshevchenko@gmail.com>
2021-03-26 11:52:12 +0300

    Append command line parsing

--- 02/hello/src/main.rs
+++ 02/hello/src/main.rs
@@ -1,5 +1,23 @@
+use std::io::Write;
+use std::str::FromStr;
+
 fn main() {
-    println!("{}", gcd(15, 21));
+    let mut numbers = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBERS ...").unwrap();
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greates common divisor of {:?} is {}", numbers, d);
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
