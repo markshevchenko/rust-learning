3588d91bce3cf2abb4c29c6864cd1db0390c0d73
Mark Shevchenko <markshevchenko@gmail.com>
2021-04-02 11:50:51 +0300

    Append method `BinaryTree<T>.add`

--- 10/sumtypes/src/main.rs
+++ 10/sumtypes/src/main.rs
@@ -128,7 +128,7 @@ fn main() {
         }
 
         if let Shape::Cuboid { corner1: Point3d { x: x1, y: y1, z: z1 },
-                               corner2: Point3d { x: x2, y: y2, z: z2 } } = shape2 {
+        corner2: Point3d { x: x2, y: y2, z: z2 } } = shape2 {
             assert_eq!(x1, 0.0);
             assert_eq!(y1, 0.0);
             assert_eq!(z1, 0.0);
@@ -177,5 +177,197 @@ fn main() {
         tree_to_vec(&tree, &mut v);
 
         assert_eq!(v, vec![1, 3, 5]);
+    }
+
+    {
+        fn match_int_constants(n: i32) {
+            match n {
+                0 => (),
+                1 => println!("The rabbit"),
+                n => println!("{} rabbits jump", n),
+            }
+        }
+
+        match_int_constants(0);
+        match_int_constants(1);
+        match_int_constants(42);
+    }
+    
+    {
+        #[derive(Debug, PartialEq)]
+        enum Color {
+            Red,
+            Green,
+            Blue,
+            Other(String),
+        }
+
+        fn parse_color(name: &str) -> Color {
+            match name {
+                "red" => Color::Red,
+                "green" => Color::Green,
+                "blue" => Color::Blue,
+                other => Color::Other(other.to_string()),
+            }
+        }
+
+        assert_eq!(parse_color("red"), Color::Red);
+        assert_eq!(parse_color("yellow"), Color::Other("yellow".to_string()));
+
+        fn color_to_str(color: Color) -> String {
+            match color {
+                Color::Other(name) => name,
+                _ => "fix color".to_string(),
+            }
+        }
+
+        assert_eq!(color_to_str(Color::Green), "fix color");
+        assert_eq!(color_to_str(Color::Other("magenta".to_string())), "magenta");
+    }
+
+    {
+        #[derive(Debug)]
+        struct Point {
+            x: i32,
+            y: i32
+        }
+
+        fn describe_point(point: Point) {
+            match point {
+                Point { x: 0, y: height } => println!("Above you on height {} meters", height),
+                Point { x, y } => println!("In point {}, {}", x, y),
+            }
+        }
+
+        describe_point(Point { x: 0, y: 32 });
+        describe_point(Point { x: 20, y: 30 });
+    }
+
+    {
+        struct Name {
+            given: String,
+            family: String,
+        }
+
+        impl Name {
+            fn to_string(self) -> String {
+                format!("{} {}", self.given, self.family)
+            }
+        }
+
+        let beatle = Name { given: "Paul".to_string(), family: "McCartney".to_string() };
+
+        match beatle {
+            Name { ref given, .. } => println!("Sir {}", given),
+        }
+
+        assert_eq!(beatle.to_string(), "Paul McCartney");
+
+        let mut big_lebovsky = Name { given: "John".to_string(), family: "Lennon".to_string() };
+
+        match big_lebovsky {
+            Name { ref mut family, .. } => if family == "Lennon" { *family = "Lenin".to_string() },
+        }
+
+        assert_eq!(big_lebovsky.to_string(), "John Lenin");
+
+        let person = Name { given: "Santa".to_string(), family: "Claus".to_string() };
+
+        match &person {
+            &Name { ref given, ref family } => println!("Spirit of {} {}", given, family),
+        }
+    }
+
+    {
+        #[derive(Debug)]
+        enum CharKind {
+            Digit,
+            Alpha,
+            Whitespace,
+            Other(char),
+        }
+
+        fn get_kind(c: char) -> CharKind {
+            match c {
+                '0'..='9' => CharKind::Digit,
+                'a'..='z' | 'A'..='Z' => CharKind::Alpha,
+                ' ' | '\n' | '\r' | '\t' => CharKind::Whitespace,
+                other => CharKind::Other(other),
+
+            }
+        }
+
+        println!("3 is {:?}", get_kind('3'));
+        println!("n is {:?}", get_kind('n'));
+        println!("P is {:?}", get_kind('P'));
+        println!("\\t is {:?}", get_kind('\t'));
+        println!("* is {:?}", get_kind('*'));
+    }
+
+    {
+        enum Shape {
+            Rect(i32, i32, i32, i32),
+            Circle(i32, i32, i32)
+        }
+
+        fn perimeter(shape: &Shape) -> f64 {
+            match shape {
+                Shape::Rect(x1, y1, x2, y2) => 2.0 * ((y2 - y1 + 1) + (x2 - x1 + 1)) as f64,
+                Shape::Circle(_, _, radius) => 2.0 * std::f64::consts::PI * (*radius as f64),
+            }
+        }
+
+        fn print_perimeter(shape: &Shape) {
+            match shape {
+                rect@Shape::Rect(..) => println!("Perimeter of rectangle is {}", perimeter(&rect)),
+                circle@Shape::Circle(..) => println!("Round of circle is {}", perimeter(&circle)),
+            }
+        }
+
+        print_perimeter(&Shape::Rect(10, 10, 40, 30));
+        print_perimeter(&Shape::Circle(30, 30, 20));
+    }
+
+    {
+        #[derive(Debug)]
+        enum BinaryTree<T> {
+            Empty,
+            NonEmpty(Box<TreeNode<T>>),
+        }
+
+        #[derive(Debug)]
+        struct TreeNode<T> {
+            element: T,
+            left: BinaryTree<T>,
+            right: BinaryTree<T>,
+        }
+
+        impl<T: Ord> BinaryTree<T> {
+            fn new() -> BinaryTree<T> { BinaryTree::Empty }
+
+            fn add(&mut self, value: T) {
+                match *self {
+                    BinaryTree::Empty => *self = BinaryTree::NonEmpty(Box::new(TreeNode {
+                        element: value,
+                        left: BinaryTree::Empty,
+                        right: BinaryTree::Empty,
+                    })),
+                    BinaryTree::NonEmpty(ref mut node) =>
+                        if value <= node.element {
+                            node.left.add(value)
+                        } else {
+                            node.right.add(value)
+                        }
+
+                }
+            }
+        }
+
+        let mut tree = BinaryTree::new();
+        tree.add(3);
+        tree.add(1);
+        tree.add(5);
+
+        println!("{:?}", tree);
     }
 }
