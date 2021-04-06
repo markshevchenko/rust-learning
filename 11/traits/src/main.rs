db48b9464ad7dea1ccd98341dd185bad75d480ec
Mark Shevchenko <markshevchenko@gmail.com>
2021-04-06 08:43:40 +0300

    Finish chapter 11

--- 11/traits/src/main.rs
+++ 11/traits/src/main.rs
@@ -1,3 +1,6 @@
+extern crate rand;
+
+use std::ops::{Add, Mul};
 use std::collections::HashMap;
 use std::fmt::Debug;
 use std::fs::File;
@@ -5,6 +8,7 @@ use std::hash::Hash;
 use std::io::Result;
 use std::io::Write;
 use std::iter::Iterator;
+use rand::{random, OsRng, Rand};
 
 fn main() {
     {
@@ -80,5 +84,223 @@ fn main() {
                           0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
         top_five(&values);
+    }
+
+    {
+        trait Math<T> {
+            fn add(&self, other: &T) -> T;
+        }
+
+        impl Math<i32> for i32 {
+            fn add(&self, other: &i32) -> i32 {
+                *self + *other
+            }
+        }
+
+        impl Math<String> for String {
+            fn add(&self, other: &String) -> String {
+                self.clone() + other
+            }
+        }
+
+        assert_eq!(2.add(&3), 5);
+        assert_eq!("foo".to_string().add(&"bar".to_string()), "foobar");
+    }
+
+    {
+        pub struct Nul;
+
+        impl Write for Nul {
+            fn write(&mut self, buf: &[u8]) -> Result<usize> {
+                Ok(buf.len())
+            }
+
+            fn flush(&mut self) -> Result<()> {
+                Ok(())
+            }
+        }
+    }
+
+    {
+        trait WriteHtml {
+            fn write_start_tag(&mut self, tag: &[u8]);
+            fn write_end_tag(&mut self, tag: &[u8]);
+        }
+
+        impl<W: Write> WriteHtml for W {
+            fn write_start_tag(&mut self, tag: &[u8]) {
+                self.write(b"<").unwrap();
+                self.write(tag).unwrap();
+                self.write(b">").unwrap();
+            }
+
+            fn write_end_tag(&mut self, tag: &[u8]) {
+                self.write(b"</").unwrap();
+                self.write(tag).unwrap();
+                self.write(b">").unwrap();
+            }
+        }
+    }
+
+    {
+        trait Math {
+            fn add(&self, other: Self) -> Self;
+        }
+
+        impl Math for i32 {
+            fn add(&self, other: i32) -> i32 {
+                *self + other
+            }
+        }
+
+        impl Math for String {
+            fn add(&self, other: String) -> String {
+                self.clone() + &other
+            }
+        }
+
+        assert_eq!(2.add(3), 5);
+        assert_eq!("foo".to_string().add("bar"), "foobar");
+    }
+
+    {
+        trait Foo {
+            fn foo(&self) -> i32;
+        }
+
+        trait Bar: Foo {
+            fn bar(&self) -> i32;
+        }
+
+        struct Baz;
+
+        impl Foo for Baz {
+            fn foo(&self) -> i32 { 32 }
+        }
+
+        impl Bar for Baz {
+            fn bar(&self) -> i32 { 42 }
+        }
+
+        let baz = Baz;
+
+        assert_eq!(baz.foo(), 32);
+        assert_eq!(baz.bar(), 42);
+    }
+
+    {
+        trait Creatable {
+            fn new() -> Self;
+        }
+
+        struct Baz;
+
+        impl Creatable for Baz {
+            fn new() -> Baz {
+                Baz
+            }
+        }
+
+        let _baz = Baz::new();
+    }
+
+    {
+        let line = "is there anybody going to listen to my story";
+        let words: Vec<String> = line.split_whitespace()
+                                     .map(<str as ToString>::to_string)
+                                     .collect();
+
+        assert_eq!(words[1], "there");
+    }
+
+    {
+        #[derive(Clone, Copy)]
+        struct Point2d<T> {
+            x: T,
+            y: T,
+        }
+
+        struct Point2dIterator<T> {
+            p: Point2d<T>,
+            index: i32
+        }
+
+        impl<T: Copy> Iterator for Point2dIterator<T> {
+            type Item = T;
+
+            fn next(&mut self) -> std::option::Option<<Self as std::iter::Iterator>::Item> {
+                self.index += 1;
+
+                match self.index {
+                    1 => Some(self.p.x),
+                    2 => Some(self.p.y),
+                    _ => None,
+                }
+            }
+        }
+
+        impl<T: Copy> IntoIterator for Point2d<T> {
+            type Item = T;
+            type IntoIter = Point2dIterator<T>;
+
+            fn into_iter(self) -> <Self as std::iter::IntoIterator>::IntoIter {
+                Self::IntoIter {
+                    p: self,
+                    index: 0,
+                }
+            }
+        }
+
+        fn dump<I>(iter: I)
+            where I: Iterator, I::Item: Debug
+        {
+            for (index, value) in iter.enumerate() {
+                println!("{}: {:?}", index, value);
+            }
+        }
+
+        let point = Point2d { x: 100, y: 200 };
+        dump(point.into_iter());
+    }
+
+    {
+        struct Multiplier {
+            multiplier: usize
+        }
+
+        impl Mul<Multiplier> for String {
+            type Output = String;
+
+            fn mul(self, m: Multiplier) -> <Self as std::ops::Mul<Multiplier>>::Output {
+                self.repeat(m.multiplier)
+            }
+        }
+
+        let foo4 = "foo".to_string() * Multiplier { multiplier: 4};
+        assert_eq!(foo4, "foofoofoofoo");
+    }
+
+    {
+        println!("{}", random::<f64>());
+
+        let mut rng = OsRng::new().unwrap();
+
+        println!("{}", f64::rand(&mut rng));
+    }
+
+    {
+        fn dot<N>(v1: &[N], v2: &[N]) -> N
+        where N: Add<Output=N> + Mul<Output=N> + Default + Copy {
+            let mut total = N::default();
+
+            for i in 0..v1.len() {
+                total = total + v1[i] * v2[i];
+            }
+
+            total
+        }
+
+        assert_eq!(dot(&[1, 2, 3, 4], &[1, 1, 1, 1,]), 10);
+        assert_eq!(dot(&[53.0, 7.0], &[1.0, 5.0]), 88.0);
     }
 }
